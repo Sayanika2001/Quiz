e5e633f81148851daab7d3af96efcd01b665a0e1
Sayanika2001 <108542048+Sayanika2001@users.noreply.github.com>
2022-07-28 11:19:21 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+    numb: 1,
+    question: "In which decade was the American Institute of Electrical Engineers (AIEE) founded??",
+    answer: "1880s",
+    options: [
+      "1850s",
+      "1880s",
+      "1905s",
+      "1945s"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is part of a database that holds only one type of information?",
+    answer: "Field",
+    options: [
+       "Report",
+       "Field",
+       "Record",
+       "File",
+     
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+];
